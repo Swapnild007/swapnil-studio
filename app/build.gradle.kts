4a31b9ebd341fb38ce6f007b957e35ae957568f4
Swapnild007 <swapneeldalvi3@gmail.com>
2026-08-25 14:34:25 +0530

    Create app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android { namespace = "com.swapnil.studio.security"; compileSdk = 35
+    defaultConfig { applicationId = "com.swapnil.studio.security"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0.0" }
+}
